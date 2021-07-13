4b31a7e0f2f66828df46aca2ba37f3e2ab4509ba
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 11:38:54 +0900

    サウンド追加、敵のAI追加。ひとまず完成

--- airhockey/Main.cpp
+++ airhockey/Main.cpp
@@ -11,7 +11,6 @@ void Main()
 	int score_Red	= 0;
 	int score_Blue	= 0;
 
-
 	Rect Stage(200, 0, 400, 600);
 
 	//パドルの可動域
@@ -28,10 +27,14 @@ void Main()
 	// パック
 	Circle Pack(400, 300, 10);
 	// 速さ
-	double speed = 10.0;
-	double cursor_speed = 0;
-		// 速度
-	Vec2 PackVelocity(0, speed);
+	double speed = 20.0;
+	// 速度
+	Vec2 PackVelocity(40 , 40);
+
+	// 音
+	const Audio audio_Hit(U"sounds/hit.wav");
+	const Audio audio_Bounce(U"sounds/bounce.wav");
+	const Audio audio_Goal(U"sounds/goal.wav");
 
 	const double deltaTime = Scene::DeltaTime();
 
@@ -61,51 +64,72 @@ void Main()
 
 		// パックの動作
 		Pack.moveBy(PackVelocity * Scene::DeltaTime());
+
+		// 壁との衝突
 		if (Pack.y < 10 && PackVelocity.y < 0 )
 		{
+			audio_Bounce.playOneShot();
 			PackVelocity.y *= -1;
 		}
 		if (Pack.y > 590 && PackVelocity.y > 0)
 		{
+			audio_Bounce.playOneShot();
 			PackVelocity.y *= -1;
 		}
 		if (Pack.x < 210 && PackVelocity.x < 0)
 		{
+			audio_Bounce.playOneShot();
 			PackVelocity.x *= -1;
 		}
 		if (Pack.x > 590 && PackVelocity.x > 0)
 		{
+			audio_Bounce.playOneShot();
 			PackVelocity.x *= -1;
 		}
 
+		// パックとパドルの衝突
 		if (Paddle_Blue.intersects(Pack) && Zone_Blue.contains(Paddle_Blue)) {
+			audio_Hit.playOneShot();
 			PackVelocity.x = (Pack.x - Paddle_Blue.x) * (Cursor::Delta().x * Cursor::Delta().y + speed);
 			PackVelocity.y = (Pack.y - Paddle_Blue.y) * (Cursor::Delta().x * Cursor::Delta().y + speed);
 		}
 		if (Paddle_Red.intersects(Pack) && Zone_Red.contains(Paddle_Red)) {
-			PackVelocity.x = (Pack.x - Paddle_Red.x) * (Cursor::Delta().x * Cursor::Delta().y + speed);
-			PackVelocity.y = (Pack.y - Paddle_Red.y) * (Cursor::Delta().x * Cursor::Delta().y + speed);
+			audio_Hit.playOneShot();
+			PackVelocity.x = (Pack.x - Paddle_Red.x) * ((Pack.x - Paddle_Red.x) * 0.1 * (Pack.y - Paddle_Red.y) * 0.1 + speed);
+			PackVelocity.y = (Pack.y - Paddle_Red.y) * ((Pack.x - Paddle_Red.x) * 0.1 * (Pack.y - Paddle_Red.y) * 0.1 + speed);
 		}
 
 		Pack.draw(Palette::White).drawFrame(2, 0, Palette::Orange);
 
 		// 青の得点
 		if (Goal_Red.intersects(Pack)) {
+			audio_Goal.playOneShot();
 			score_Blue += 1;
 			Pack.setPos(400,200);
-			PackVelocity.set(-20, -20);
+			PackVelocity.set(-40, -40);
 		}
 
 		// 赤の得点
 		if (Goal_Blue.intersects(Pack)) {
+			audio_Goal.playOneShot();
 			score_Red += 1;
 			Pack.setPos(400, 400);
-			PackVelocity.set(20, 20);
+			PackVelocity.set(40, 40);
 		}
 
 		// 敵のAI
-		// Paddle_Red.x = Pack.x - Paddle_Red.x * deltaTime * 0.1;
-		// Paddle_Red.y = Pack.y - Paddle_Red.y * deltaTime * 0.1;
+
+		if (Zone_Red.contains(Pack)) {
+			Paddle_Red.moveBy((Pack.x - Paddle_Red.x) * 0.1, (Pack.y - Paddle_Red.y) * 0.1);
+		}
+		else
+		{
+			Paddle_Red.moveBy((400 - Paddle_Red.x) * 0.01, (100 - Paddle_Red.y) * 0.01);
+		}
+
+
+		// Paddle_Red.x += (Pack.x - Paddle_Red.x) * deltaTime;
+		// Paddle_Red.y += (Pack.y - Paddle_Red.y) * deltaTime;
 
 		// [A] キーが押されたら
 		if (KeyA.down())
